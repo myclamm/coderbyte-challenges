772a57bf66fe922c38ad41d74029119f2bfa42c5
Michael Lam <mikelam231@gmail.com>
2014-04-07 18:18:37 -0400

    Update 17 - ArrayAdditionI.js

--- 17 - ArrayAdditionI.js
+++ 17 - ArrayAdditionI.js
@@ -1,18 +1,17 @@
 function ArrayAdditionI(arr) {
+//First sort the array and define the largest number    
     arr.sort(function(a,b){return a-b})
-    var largest = arr.pop()//first define largest
-    k =0 //sets k to level 0
-    v = 1 //sets v to 0?
+    var largest = arr.pop()
+    k =0
+    v = 1 
     t = 0
     sum = 0;
     sum2 = 0;
-    console.log("largest: "+largest)//LOG TEST
-    console.log("arr: "+arr)//LOG TEST
+    //for every number in the array except the last one, create a level 0 pair of arr[k]+arr[i]
+    //if arr = [1,2,3,4] the initial pairs will be 1+2, then 1+3, etc. Pairs are stored in the sum variable
     for (i=0;i<=arr.length-2;i++) {
-        sum = arr[k]+arr[i+1]; // sets level 0 pairs e.g. 1+2, 1+3, 1+4
-        
-        console.log("i: "+i)
-        console.log("sum: "+sum)//LOG TEST
+        sum = arr[k]+arr[i+1]; // sets level 0 pairs e.g. 1+2, 1+3, 1+4. K starts at zero
+
         if(sum==largest) { return true}; // tests level 0 pairs against largest
         while (v<=arr.length-2) {
             for (j=v+1;j<=arr.length-1;j++) {
